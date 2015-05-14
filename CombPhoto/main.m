70f124afd554bd6f8c5a38ad41e25f407756eaeb
Edward <ObesityChow@Gmail.com>
2015-05-14 16:37:20 +0800

    fix the bug that the output pic to be full black when the number of file is too big

--- CombPhoto/main.m
+++ CombPhoto/main.m
@@ -8,38 +8,51 @@
 
 #import <Foundation/Foundation.h>
 #import <CoreGraphics/CoreGraphics.h>
+void ScanFiles(NSFileManager * manager,NSMutableArray * listFile,NSString * rootPath,NSError ** error)
+{
+    NSArray * array = [manager contentsOfDirectoryAtPath:rootPath error:error];
+    for(NSString * s in array)
+    {
+        NSString * path = [NSString stringWithFormat:@"%@/%@",rootPath,s];
+        BOOL isDir = NO;
+        [manager fileExistsAtPath:path isDirectory:&isDir];
+        if(!isDir)
+        {
+            
+            NSString *p = [path substringFromIndex:path.length - 4];
+            if([[p uppercaseString] compare:@".JPG"] == NSOrderedSame)
+                [listFile addObject:path];
+        }
+        else
+        {
+            ScanFiles(manager,listFile,path,error);
+        }
+    }
+}
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         NSFileManager * manager = [NSFileManager defaultManager];
-        NSError * error;
-        NSArray *templist = [manager contentsOfDirectoryAtPath:[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding] error:&error];
+        NSMutableArray * fileList = [[NSMutableArray alloc] init];
+                NSError * error;
+        ScanFiles(manager,fileList,[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding],&error);
+        
+
         if(error)
         {
             NSLog(@"%@",error);
             return 0;
         }
-        NSMutableArray * fileList = [[NSMutableArray alloc] init];
-        for(NSString * s in templist)
-        {
-            NSString * path = [NSString stringWithFormat:@"%@%@",[NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding],s];
-            BOOL isDir = NO;
-            [manager fileExistsAtPath:path isDirectory:&isDir];
-            if(!isDir)
-            {
-                
-                NSString *p = [path substringFromIndex:path.length - 4];
-                if([[p uppercaseString] compare:@".JPG"] == NSOrderedSame)
-                    [fileList addObject:path];
-            }
-        }
+        
+        NSLog(@"%@",fileList);
+
+        
 
         
         
         
         CGImageRef image;
         CGContextRef finalContext,tempContext;
-        
         CGDataProviderRef  provider;
         provider = CGDataProviderCreateWithFilename([(NSString *)fileList[0] cStringUsingEncoding:NSUTF8StringEncoding]);
         
@@ -54,7 +67,7 @@ int main(int argc, const char * argv[]) {
         
         unsigned char * data = CGBitmapContextGetData(finalContext);
         
-        
+        double * cache = malloc(sizeof(double) * 4 * w * h);
         for(int i = 0; i < fileList.count;++i )
         {
 
@@ -67,7 +80,7 @@ int main(int argc, const char * argv[]) {
             CGRect rect = CGRectMake(0, 0, w, h);
             CGContextDrawImage(tempContext , rect , image);
             unsigned char * prodata = CGBitmapContextGetData(tempContext);
-          
+            
             ///////
             int wOff = 0;
             int pixOff = 0;
@@ -79,22 +92,50 @@ int main(int argc, const char * argv[]) {
                     int green = (unsigned char)prodata[pixOff+1];
                     int blue = (unsigned char)prodata[pixOff+2];
                     
+                    /*
                     data[pixOff] += red / (double)fileList.count;
                     data[pixOff+1] += green / (double)fileList.count;
                     data[pixOff+2] += blue / (double)fileList.count;
+                    */
                     
-                    //NSLog(@"%d,%d,%d \t",(int)(red / 9.0f),green,blue);
+                    cache[pixOff] += (double)red / fileList.count;
+                    cache[pixOff+1] += (double)green / fileList.count;
+                    cache[pixOff+2] += (double)blue / fileList.count;
+                    //NSLog(@"%lf,%d,%d \t",cache[pixOff],green,blue);
                     
                     pixOff += 4;
                 }
                 wOff += w * 4;
             }
             //////
+            
             NSLog(@"%2.1lf%%\n",(double)i/fileList.count*100.0f);
             CFRelease(provider);
             CFRelease(image);
             CFRelease(tempContext);
             
+        }
+        
+        int wOff = 0;
+        int pixOff = 0;
+        for(size_t y = 0;y<h;y++)
+        {
+            for(size_t x = 0; x < w;++x)
+            {
+                int red = (unsigned char)cache[pixOff];
+                int green = (unsigned char)cache[pixOff+1];
+                int blue = (unsigned char)cache[pixOff+2];
+                
+                
+                data[pixOff] = red;
+                data[pixOff+1] = green;
+                data[pixOff+2] = blue;
+            
+                //NSLog(@"%lf,%d,%d \t",cache[pixOff],green,blue);
+                
+                pixOff += 4;
+            }
+            wOff += w * 4;
         }
         
         CGImageRef finalimage = CGBitmapContextCreateImage(finalContext);
@@ -112,6 +153,7 @@ int main(int argc, const char * argv[]) {
         
         [dddd writeToFile:[NSString stringWithCString:argv[2] encoding:NSUTF8StringEncoding] atomically:YES];
     
+        NSLog(@"DONE...\nThe picture");
     }
     return 0;
 }
